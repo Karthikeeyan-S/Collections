6494235ab7f381f02e9442a7b30fd217425b0329
Karthik <karthikeeyan.srinivasan@gmail.com>
2025-02-06 15:27:07 +0530

    Arraylist

---
+++ ArraylistDemo.java
@@ -0,0 +1,21 @@
+package collectionp;
+
+import java.util.*;
+
+class ArraylistDemo
+{
+	public static void main(String [] args)
+	{
+		ArrayList<String> a1 = new ArrayList<String>();
+		System.out.println("Default Size of an arraylist:"+a1.size());
+		a1.add("Karthikeeyan");
+		a1.add("Manasvini");
+		a1.add("Keshav");
+		a1.add("Ashwini");
+		a1.add("Palvesh");
+		System.out.println(a1);
+		System.out.println(a1);
+			
+		
+	}
+}
